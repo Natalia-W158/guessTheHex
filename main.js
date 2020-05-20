9f7382f9d71dfa9e9eaf86ac36d1ea254c4ce248
Natalia- W158 <you@example.com>
2020-05-20 19:59:07 +0200

    correct box

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
+let randomColor = () => "#" + Math.floor(Math.random()*16777215).toString(16);
+
+let match;
+
 
 
 function start() {
-    let randomColor = () => "#" + Math.floor(Math.random()*16777215).toString(16);
+    /* let randomColor = () => "#" + Math.floor(Math.random()*16777215).toString(16); */
     let randomBox = document.getElementById("randomBox");
     randomBox.innerHTML = randomColor();
     
-
+    match = Math.floor(Math.random()*4);
+    
     let boxes = document.getElementsByClassName("box");
-    for (let box of boxes){
-        box.style.backgroundColor = randomColor();
+    for (let i=0; i < boxes.length; i++){
+        boxes[i].style.backgroundColor = i == match ? randomBox.innerHTML: randomColor();
+
     }
 
-}
+};
 
 function compareColorCode(x){
-    let randomColor = () => "#" + Math.floor(Math.random()*16777215).toString(16);
-    if( x === randomColor()){
-        randomBox.innerHTML= "You're the Greatest";
+console.log(x,match)
+    if( x === match+1){
+        alert("You're the Greatest");
     } else {
         randomBox.innerHTML ="Try again";
     }
-    
-}
+};
 
 
 
